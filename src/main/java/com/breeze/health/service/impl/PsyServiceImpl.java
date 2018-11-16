973c71404673acf4289b0dc6f32bdaf58685a8c1
jiemaoxue <jiemx2008@163.com>
2018-11-16 15:09:29 +0800

    zisha report

--- src/main/java/com/breeze/health/service/impl/PsyServiceImpl.java
+++ src/main/java/com/breeze/health/service/impl/PsyServiceImpl.java
@@ -1,5 +1,6 @@
 package com.breeze.health.service.impl;
 
+import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -12,13 +13,8 @@ import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import com.breeze.health.beans.vo.Result;
-import com.breeze.health.beans.vo.UserPhysiologicalVo;
 import com.breeze.health.beans.vo.UserPsySuicideVo;
 import com.breeze.health.beans.vo.UserPsychologicalVo;
-import com.breeze.health.beans.vo.UserPsychologicalVo;
-import com.breeze.health.beans.vo.UserPsychologicalVo;
-import com.breeze.health.entity.UserPhysiological;
-import com.breeze.health.entity.UserPhysiologicalExample;
 import com.breeze.health.entity.UserPsychological;
 import com.breeze.health.entity.UserPsychologicalAD8;
 import com.breeze.health.entity.UserPsychologicalAD8Example;
@@ -334,22 +330,37 @@ public class PsyServiceImpl implements PsyService{
 					if (gad7.getGad7Score()>=5 && gad7.getGad7Score()<=9)
 					{
 						//可能有轻微焦虑症
-						psychologicalReport += "<p>经初步测试，结果提示您可能存在轻微焦虑症，您可以通过相关链接了解更多的相关信息，并进行适当的自我调节，若有疑问或需要更进一步的帮助，请与您的签约医生或社区护士联系。</p>";
+						psychologicalReport += "<p>经初步测试，结果提示您可能存在轻微焦虑情绪。</p>";
+						psychologicalReport += "<p>对于此，您不用过分紧张，即便是正常人群也可能存在焦虑情绪。焦虑是对亲人或自己生命安全、前途命运等的过度担心而产生的一种烦躁情绪。其中含有着急、挂念、忧愁、紧张、恐慌、不安等成分。它与危急情况和难以预测、难以应付的事件有关。事过境迁、焦虑就可能解除。有人并无客观原因而长期处于焦虑状态，常常无缘无故害怕大祸临头，担心患有不可救药的严重疾病，以致出现坐卧不宁、惶惶不安等症状。这种异常焦虑，才称之为焦虑症。首先您自省一下，是否有一些引发您产生焦虑情绪的原因，如慢性病的影响，不良的生活事件，个性特点等，通过分析引起焦虑的原因，进行有针对性的干预。平日您可以做一些放松训练来缓解焦虑情绪。</p>";
+						
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/ev0TRzsur1aVvFT7gpgFyw\">情绪管理的常用方法</a></p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/GT0IrMpLNuzvg49dpq54fA\">焦虑情绪调整方法</a></p>";
 					}else if (gad7.getGad7Score()>=10 && gad7.getGad7Score()<=13)
 					{
 						//可能有中度焦虑症
-						psychologicalReport += "<p>经初步测试，结果提示您可能存在中度焦虑症，您可以通过相关链接了解更多的相关信息，并进行适当的自我调节，若有疑问或需要更进一步的帮助，请与您的签约医生或社区护士联系。</p>";
+						psychologicalReport += "<p>经初步测试，结果提示您可能存在中度焦虑情绪。</p>";
+						psychologicalReport += "<p>对于此，您不用过分紧张，即便是正常人群也可能存在焦虑情绪。焦虑是对亲人或自己生命安全、前途命运等的过度担心而产生的一种烦躁情绪。其中含有着急、挂念、忧愁、紧张、恐慌、不安等成分。它与危急情况和难以预测、难以应付的事件有关。事过境迁、焦虑就可能解除。有人并无客观原因而长期处于焦虑状态，常常无缘无故害怕大祸临头，担心患有不可救药的严重疾病，以致出现坐卧不宁、惶惶不安等症状。这种异常焦虑，才称之为焦虑症。</p>";
+						psychologicalReport += "<p>首先您自省一下，是否有一些引发您产生焦虑情绪的原因，如慢性病的影响，不良的生活事件，个性特点等，针对引起焦虑的原因，可以进行针对性的干预。平日您可以做一些放松训练。</p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/ev0TRzsur1aVvFT7gpgFyw\">情绪管理的常用方法</a></p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/GT0IrMpLNuzvg49dpq54fA\">焦虑情绪调整方法</a></p>";
+						psychologicalReport += "<p如果不能缓解，可求助专业的心理医生进行心理治疗，临床心理医师通过言语或非言语沟通，建立起良好的医患关系，应用有关心理学和医学的专业知识，引导和帮助患者改变行为习惯、认知应对方式等。音乐治疗，舞动治疗，陶艺治疗，美术治疗，阅读治疗等等都可以有效缓解焦虑情绪。</p>";
 					}else if (gad7.getGad7Score()>=14 && gad7.getGad7Score()<=18)
 					{
 						//可能有中重度焦虑症
-						psychologicalReport += "<p>经初步测试，结果提示您可能存在中重度焦虑症，您可以通过相关链接了解更多的相关信息，并进行适当的自我调节，若有疑问或需要更进一步的帮助，请与您的签约医生或社区护士联系。</p>";
+						psychologicalReport += "<p>经初步测试，结果提示您可能存在中重度焦虑情绪。</p>";
+						psychologicalReport += "<p>对于此，您不用过分紧张，即便是正常人群也可能存在一定的焦虑情绪。焦虑是对亲人或自己生命安全、前途命运等的过度担心而产生的一种烦躁情绪。其中含有着急、挂念、忧愁、紧张、恐慌、不安等成分。它与危急情况和难以预测、难以应付的事件有关。事过境迁、焦虑就可能解除。有人并无客观原因而长期处于焦虑状态，常常无缘无故害怕大祸临头，担心患有不可救药的严重疾病，以致出现坐卧不宁、惶惶不安等症状。这种异常焦虑，才称之为焦虑症。首先您自省一下，是否有一些引发您产生焦虑情绪的原因，如慢性病的影响，不良的生活事件，个性特点等，针对引起焦虑的原因，可以进行有针对性的区分。</p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/ev0TRzsur1aVvFT7gpgFyw\">情绪管理的常用方法</a></p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/GT0IrMpLNuzvg49dpq54fA\">焦虑情绪调整方法</a></p>";
 					}else if (gad7.getGad7Score()>=19)
 					{
 						//可能有重度焦虑症
-						psychologicalReport += "<p>经初步测试，结果提示您可能存在重度焦虑症，您可以通过相关链接了解更多的相关信息，并进行适当的自我调节，若有疑问或需要更进一步的帮助，请与您的签约医生或社区护士联系。</p>";
+						psychologicalReport += "<p>经初步测试，结果提示您可能存在重度焦虑情绪。</p>";
+						psychologicalReport += "<p>对于此，您不用过分紧张，即便是正常人群也可能存在焦虑情绪。焦虑是对亲人或自己生命安全、前途命运等的过度担心而产生的一种烦躁情绪。其中含有着急、挂念、忧愁、紧张、恐慌、不安等成分。它与危急情况和难以预测、难以应付的事件有关。事过境迁、焦虑就可能解除。有人并无客观原因而长期处于焦虑状态，常常无缘无故害怕大祸临头，担心患有不可救药的严重疾病，以致出现坐卧不宁、惶惶不安等症状。这种异常焦虑，才称之为焦虑症。首先您自省一下，是否有一些引发您产生焦虑情绪的原因，如慢性病的影响，不良的生活事件，个性特点等，针对引起焦虑的原因，可以进行有针对性的区分。</p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/ev0TRzsur1aVvFT7gpgFyw\">情绪管理的常用方法</a></p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/GT0IrMpLNuzvg49dpq54fA\">焦虑情绪调整方法</a></p>";
 					}else
 					{
-						psychologicalReport += "<p>经初步测试，结果提示您情绪状态良好，没有焦虑情绪。</p>";
+						psychologicalReport += "<p>经初步测试，结果提示您情绪状态良好，没有焦虑情绪!</p>";
 					}
 				}
 				if (phq9!=null)
@@ -357,21 +368,33 @@ public class PsyServiceImpl implements PsyService{
 					if (phq9.getPhq9Score()>=5 && phq9.getPhq9Score()<=9)
 					{
 						//轻度抑郁
-						psychologicalReport += "<p>经初步测试，结果提示您可能存在轻度抑郁，您可以通过相关链接了解更多的相关信息，并进行适当的自我调节，若有疑问或需要更进一步的帮助，请与您的签约医生或社区护士联系。</p>";
+						psychologicalReport += "<p>您可能存在轻度抑郁。</p>";
+						psychologicalReport += "<p>抑郁是一种很常见的情绪反应，特别是现代社会，相对比精神心理，物质生活已经极大丰富，人们也更加关注自己的内心世界，可能内心的满足感与成就感就有所欠缺。当人们遇到工作压力、生活挫折、痛苦境遇、生老病死、天灾人祸等情况时，可能就会产生抑郁情绪。几乎每个人都可能会在某个时候觉得情绪低落。抑郁是指以心境低落为主的精神状态。常伴有各种症状，如焦虑、激越、无价值感、无助感、无望感、自杀观念、意志减退、精神运动迟滞，及各种躯体症状和生理机能障碍（如睡眠障碍）。请结合自身的情况，反思自身的抑郁情绪是否有一定的诱因，如果存在诱因，我们可以通过祛除诱因来达到缓解抑郁情绪的目的。如果诱因无法祛除或生活事件解决不了，尽可能寻求周围的社会支持系统获取帮助。抑郁情绪并不稀奇，也不可怕，可称之为“心灵的感冒”，每个人一生中的不同时段，都可能出现抑郁情绪。轻中度抑郁情绪无需药物治疗，只要及时调整、疏导，改变刺激环境，大多可以缓解。</p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/ev0TRzsur1aVvFT7gpgFyw\">情绪管理的常用方法</a></p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/GT0IrMpLNuzvg49dpq54fA\">抑郁情绪调整方法</a></p>";
 					}else if (phq9.getPhq9Score()>=10 && phq9.getPhq9Score()<=14)
 					{
 						//中度抑郁
-						psychologicalReport += "<p>经初步测试，结果提示您可能存在中度抑郁，您可以通过相关链接了解更多的相关信息，并进行适当的自我调节，若有疑问或需要更进一步的帮助，请与您的签约医生或社区护士联系。</p>";
+						psychologicalReport += "<p>您可能存在中度抑郁。</p>";
+						psychologicalReport += "<p>抑郁是一种很常见的情绪反应，特别是现代社会，相对比精神心理，物质生活已经极大丰富，人们也更加关注自己的内心世界，可能内心的满足感与成就感就有所欠缺。当人们遇到工作压力、生活挫折、痛苦境遇、生老病死、天灾人祸等情况时，可能就会产生抑郁情绪。几乎每个人都可能会在某个时候觉得情绪低落。抑郁是指以心境低落为主的精神状态。常伴有各种症状，如焦虑、激越、无价值感、无助感、无望感、自杀观念、意志减退、精神运动迟滞，及各种躯体症状和生理机能障碍（如睡眠障碍）。请结合自身的情况，反思自身的抑郁情绪是否有一定的诱因，如果存在诱因，我们可以通过祛除诱因来达到缓解抑郁情绪的目的。如果诱因无法祛除或生活事件解决不了，尽可能寻求周围的社会支持系统获取帮助。抑郁情绪并不稀奇，也不可怕，可称之为“心灵的感冒”，每个人一生中的不同时段，都可能出现抑郁情绪。轻中度抑郁情绪无需药物治疗，只要及时调整、疏导，改变刺激环境，大多可以缓解。</p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/ev0TRzsur1aVvFT7gpgFyw\">情绪管理的常用方法</a></p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/GT0IrMpLNuzvg49dpq54fA\">抑郁情绪调整方法</a></p>";
 					}else if (phq9.getPhq9Score()>=15 && phq9.getPhq9Score()<=19)
 					{
 						//中重度抑郁
-						psychologicalReport += "<p>经初步测试，结果提示您可能存在中重度抑郁，您可以通过相关链接了解更多的相关信息，并进行适当的自我调节，若有疑问或需要更进一步的帮助，请与您的签约医生或社区护士联系。</p>";
+						psychologicalReport += "<p>您可能存在中重度抑郁。</p>";
+						psychologicalReport += "<p>抑郁是一种很常见的情绪反应，特别是现代社会，相对比精神心理，物质生活已经极大丰富，人们也更加关注自己的内心世界，可能内心的满足感与成就感就有所欠缺。当人们遇到工作压力、生活挫折、痛苦境遇、生老病死、天灾人祸等情况时，可能就会产生抑郁情绪。几乎每个人都可能会在某个时候觉得情绪低落。抑郁是指以心境低落为主的精神状态。常伴有各种症状，如焦虑、激越、无价值感、无助感、无望感、自杀观念、意志减退、精神运动迟滞，及各种躯体症状和生理机能障碍（如睡眠障碍）。请结合自身的情况，反思自身的抑郁情绪是否有一定的诱因，如果存在诱因，我们可以通过祛除诱因来达到缓解抑郁情绪的目的。如果诱因无法祛除或生活事件解决不了，尽可能寻求周围的社会支持系统获取帮助。</p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/ev0TRzsur1aVvFT7gpgFyw\">情绪管理的常用方法</a></p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/GT0IrMpLNuzvg49dpq54fA\">抑郁情绪调整方法</a></p>";
 					}else if(phq9.getPhq9Score()>=20){
 						//重度抑郁
-						psychologicalReport += "<p>经初步测试，结果提示您可能存在重度抑郁，您可以通过相关链接了解更多的相关信息，并进行适当的自我调节，若有疑问或需要更进一步的帮助，请与您的签约医生或社区护士联系。</p>";
+						psychologicalReport += "<p>您可能存在重度抑郁。</p>";
+						psychologicalReport += "<p>抑郁是一种很常见的情绪反应，特别是现代社会，相对比精神心理，物质生活已经极大丰富，人们也更加关注自己的内心世界，可能内心的满足感与成就感就有所欠缺。当人们遇到工作压力、生活挫折、痛苦境遇、生老病死、天灾人祸等情况时，可能就会产生抑郁情绪。几乎每个人都可能会在某个时候觉得情绪低落。抑郁是指以心境低落为主的精神状态。常伴有各种症状，如焦虑、激越、无价值感、无助感、无望感、自杀观念、意志减退、精神运动迟滞，及各种躯体症状和生理机能障碍（如睡眠障碍）。请结合自身的情况，反思自身的抑郁情绪是否有一定的诱因，如果存在诱因，我们可以通过祛除诱因来达到缓解抑郁情绪的目的。如果诱因无法祛除或生活事件解决不了，尽可能寻求周围的社会支持系统获取帮助。下面介绍一些关于抑郁症的知识，希望对您有所帮助。</p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/ev0TRzsur1aVvFT7gpgFyw\">情绪管理的常用方法</a></p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/GT0IrMpLNuzvg49dpq54fA\">抑郁情绪调整方法</a></p>";
 					}else 
 					{
-						psychologicalReport += "<p>经初步测试，结果提示您情绪状态良好，没有抑郁情绪。";
+						psychologicalReport += "<p>您情绪状态良好，没有抑郁情绪！</p>";
 					}
 				}
 				
@@ -381,10 +404,15 @@ public class PsyServiceImpl implements PsyService{
 					if (ad8.getAd8Score()>=2)
 					{
 						//认知功能受损
-						psychologicalReport += "<p>经初步测试，结果提示您可能存在认知功能受损，您可以通过相关链接了解更多的相关信息，并进行适当的自我调节，若有疑问或需要更进一步的帮助，请与您的签约医生或社区护士联系。</p>";
+						psychologicalReport += "<p>经初步测试，结果提示您可能存在认知功能受损，应尽快到当地医院的认知门诊做进一步的检查，以明确诊断和及时治疗。</p>";
+						psychologicalReport += "<p>认知功能是指人脑加工、储存和提取信息的能力，包括记忆力、注意力、定向力、计算力、理解力、判断力、语言能力等，是人对外界事物的全面感知的能力。认知功能障碍泛指各种原因导致的不同程度的认知功能损害，包括轻度认知功能障碍和痴呆。</p>";
+						psychologicalReport += "<p>老年人随着年龄的增长，大脑也逐渐在老化，加之各种慢性病，尤其是心脑血管疾病的影响，发生认知功能损害的风险增加。专家建议，中年人和老年人都需要有意识地进行大脑的认知功能训练。</p>";
+						psychologicalReport += "<p>您可以参考以下信息进行相关的认知功能训练。</p>";
+						psychologicalReport += "<p><a href=\"https://mp.weixin.qq.com/s/yOtpbvqq0VfAq8KDOlVuxQ\">认知功能训练</a></p>";
 					}else
 					{
-						psychologicalReport += "<p>经初步测试，您认知功能正常。</p>";
+						psychologicalReport += "<p>您认知功能正常。</p>";
+						psychologicalReport += "<p>老年人随着年龄的增长，大脑也逐渐在老化，加之各种慢性病，尤其是心脑血管疾病的影响，发生认知功能损害的风险增加。专家建议，中年人和老年人都需要有意识地进行大脑的认知功能训练。</p>";
 					}
 				}
 				vo.setResultTitle("情绪认知测评结果");
@@ -442,6 +470,71 @@ public class PsyServiceImpl implements PsyService{
 			{
 				UserPsySuicideVo vo = new UserPsySuicideVo();
 				BeanUtils.copyProperties(suicides.get(0), vo);
+				
+				int scoreF1 = null2ZeroReverse(vo.getQ1()) + null2ZeroReverse(vo.getQ7()) + null2ZeroReverse(vo.getQ12()) + null2zero(vo.getQ17()) + null2zero(vo.getQ19()) + null2ZeroReverse(vo.getQ22()) + null2zero(vo.getQ23()) +  null2zero(vo.getQ26()) + null2zero(vo.getQ29());
+				int scoreF2 = null2zero(vo.getQ2())+ null2ZeroReverse(vo.getQ3()) + null2ZeroReverse(vo.getQ8()) + null2zero(vo.getQ9()) + null2zero(vo.getQ13())+ null2ZeroReverse(vo.getQ14())+ null2ZeroReverse(vo.getQ18())+ null2ZeroReverse(vo.getQ20())+ null2zero(vo.getQ24())+ null2zero(vo.getQ25());
+				int scoreF3 = null2zero(vo.getQ4()) + null2zero(vo.getQ6()) + null2ZeroReverse(vo.getQ10()) + null2ZeroReverse(vo.getQ15())+ null2ZeroReverse(vo.getQ28());
+				int scoreF4 = null2zero(vo.getQ5()) + null2ZeroReverse(vo.getQ11()) + null2zero(vo.getQ16()) + null2zero(vo.getQ21()) + null2zero(vo.getQ27());
+				
+				Float f1 = scoreF1/9f;
+				Float f2 = scoreF2/10f;
+				Float f3 = scoreF3/5f;
+				Float f4 = scoreF4/5f;
+				
+				String f1t = "";
+				String f2t = "";
+				String f3t = "";
+				String f4t = "";
+				if (f1<=2.5)
+					f1t = "肯定、认可、理解和宽容";
+				else if(f1>2.5 && f1<3.5)
+					f1t = "矛盾或中立";
+				else
+					f1t = "反对、否定、排斥和歧视";
+				
+				if (f2<=2.5)
+					f2t = "肯定、认可、理解和宽容";
+				else if(f2>2.5 && f2<3.5)
+					f2t = "矛盾或中立";
+				else
+					f2t = "反对、否定、排斥和歧视";
+				
+				if (f3<=2.5)
+					f3t = "肯定、认可、理解和宽容";
+				else if(f3>2.5 && f3<3.5)
+					f3t = "矛盾或中立";
+				else
+					f3t = "反对、否定、排斥和歧视";
+				
+				if (f4<=2.5)
+					f4t = "肯定、认可、理解和宽容";
+				else if(f4>2.5 && f4<3.5)
+					f4t = "矛盾或中立";
+				else
+					f4t = "反对、否定、排斥和歧视";
+				
+				DecimalFormat decimalFormat=new DecimalFormat(".00");
+				String report = "";
+				report += "<p>关于对自杀行为性质的认识: "+scoreF1+"(均分："+decimalFormat.format(f1)+" ) ，您对自杀行为持"+f1t+"的态度；</p>";
+				report += "<p>关于对自杀者的态度: "+scoreF2+"(均分："+decimalFormat.format(f2)+" )，您对自杀者持"+f2t+"的态度；</p>";
+				report += "<p>关于对自杀者家属的态度: "+scoreF3+"(均分："+decimalFormat.format(f3)+" )，您对自杀者家属持"+f3t+"的态度；</p>";
+				report += "<p>关于对安乐死的态度: "+scoreF1+"(均分： "+decimalFormat.format(f4)+")，您对安乐死持"+f4t+"的态度。</p>";
+
+				report += "<p>自杀是人类社会普遍存在的一种行为。在人类历史进程中，人们受社会发展水平、宗教思想、文化背景等的影响，对自杀的态度也有所不同，但一般来说，自杀行为被认为有悖于社会基本的伦理道德，对社会现实产生极其消极的负面影响。从个人来讲，每个人的生命都是极其珍贵的，我们应该对人生抱有积极乐观的态度，在克服一个个的困难、挫折和挑战中不断成长。自杀行为不仅威胁到实施者本人的生命安全，还会给周围的亲人朋友造成心理上的伤害。 </p>";
+				report += "<p>导致产生自杀意念的原因很多，其中抑郁症是当今社会自杀的最主要原因之一。 </p>";
+				report += "<p>“不向外界求助，独自在死胡同里打转”是抑郁症发病及病情加重的重要因素之一。70%的抑郁症患者曾有过自杀的念头，有15% 死于自杀。当我们听到某位明星因抑郁症而自杀时，常常会很疑惑，他患有抑郁症多年，为什么没有被发现呢？这是因为有些抑郁症属于隐形抑郁，其中有一种被称为“微笑型抑郁”，多发于身份高、学识高、事业有成的人群。患者的内心感到极度压抑，表面却若无其事，面带微笑。这类患者在人群中看起来和平时一样，可能会出现效率下降、精力差等情况。但他独处时，就会特别沉默。这是因为心理疾病本身有一定的主观性和隐秘性。抑郁症的核心是主观痛苦，如果没有严重到破坏社会功能，别人就很难察觉出来。一个人独处时才是抑郁暴露得最充分的时候。当他离开人群，从人群中回归到一个人孤独的状态时，突然觉得太痛苦了，活得太累，可能就会选择走极端。 </p>";
+				report += "<p>预防自杀坚持“七要七不要”：要重视不要忽视；要讨论不要躲避；要理解不要说教；要帮助不要刺激；要陪伴不要独处；要保护不要隐瞒；要治疗不要等待。当感到自己压力过大或无原因长时间心情不愉快，莫名烦躁时，应向身边的亲人、朋友求助。可通过暂时放下工作、听听音乐、与家人聚会或则出去旅游等不同方式缓解压力。 </p>";
+				report += "<p>当一个人出现自杀意念时，可尝试以下缓解方法： </p>";
+				report += "<p>1. 勇于讨论自杀话题。谈论就是一种支持，躲避反而让人绝望。 </p>";
+				report += "<p>2. 寻求共情与理解。  </p>";
+				report += "<p>3. 有人陪伴，陪伴本身就是一种良好的支持。 </p>";
+				report += "<p>4. 利用社会支持系统，主动寻求帮助。  </p>";
+				report += "<p>5.及时治疗干预，时间不是缓解自杀意念的良药，等待只能延误时机。 </p>";
+				report += "<p>如果通过这些方法依旧没有得到有效的改善，那么就要去寻求专业人员的帮助。如果只是心理方面的因素，比如：家庭纠纷、工作压力、不良事件的影响，完全是可以通过心理治疗进行干预的。经过一段时间的干预治疗后，基本可以让人能认识自我、提高与人沟通能力、促进心理成长，提升自尊感等。而如果是患有精神障碍方面的问题，可通过药物治疗辅助心理治疗的方式消除疾病。早发现早治疗，治愈率是特别高的。 </p>";
+				
+				vo.setResultTitle("自杀态度测评结果");
+				vo.setResultMsg(report);
+				
 				ret.setSuccess(true);
 				ret.setData(vo);
 			}else
@@ -455,5 +548,17 @@ public class PsyServiceImpl implements PsyService{
 		}
 		return ret;
 	}
-
+	private int null2zero(Integer val)
+	{
+		if (val == null)
+			return 0;
+		else
+			return val;
+	}
+	private int null2ZeroReverse(Integer val) {
+		if (val == null)
+			return 0;
+		else
+			return 6-val;
+	}
 }
