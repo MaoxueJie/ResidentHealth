b785df6e40e04429fc87aaccf51c088398562b54
Administrator <Administrator@LAPTOP-4SBQP3JH>
2019-04-10 22:09:03 +0800

    +1

--- src/main/resources/public/static/js/login.js
+++ src/main/resources/public/static/js/login.js
@@ -74,12 +74,13 @@ $(document).ready(function(){
 			$('.btn_vcode').addClass('enable');
 		},
 		login: function(){
+			
+			var that = this,tel = $.trim( $('#mobile').val() ),vcode = $('#vcodeForget').val();
 			if (!$('#agree').attr("checked"))
 			{
 				that.showError('','请先阅读并同意《调查知情同意书》');
 				return false;
 			}
-			var that = this,tel = $.trim( $('#mobile').val() ),vcode = $('#vcodeForget').val();
 			$.ajax({
 				url: '/cfd/login',
 				type: 'post',
