aeb9c30eed8fd95eaad266aeb60fb185ee2ccaa7
jiemaoxue <jiemx2008@163.com>
2019-06-05 09:59:46 +0800

    test

--- src/main/java/com/breeze/health/controller/AppController.java
+++ src/main/java/com/breeze/health/controller/AppController.java
@@ -1,12 +1,23 @@
 package com.breeze.health.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.breeze.health.beans.vo.Result;
+import com.breeze.health.beans.vo.UserLivingVo;
+import com.breeze.health.beans.vo.UserVo;
 
 @Controller
 @SuppressWarnings("all")
 @RequestMapping("/app")
 public class AppController {
 
-	
+	@RequestMapping(value="/")
+	@ResponseBody
+	public String test(HttpServletRequest request,UserLivingVo vo){
+		return "aaaaaaaaaaaaaaaa";
+	}
 }
